7b1752bd237923cd3a0ea9766ffbd0b7de327aef
Heba Sayed <hebasayed1603@gmail.com>
2024-05-05 22:55:41 -0400

    copy color

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const colorPickerBtn = document.querySelector("#color-picker");
 const colorList = document.querySelector(".all-colors");
-const pickedColors = JSON.parse(localStorage.getItem("picked-colors")) || "[]";
+const pickedColors = JSON.parse(localStorage.getItem("picked-colors")) || [];
+const copyColor = (elem) => {
+  navigator.clipboard.writeText(elem.dataset.color);
+  elem.innerText = "Copied";
+  setTimeout(() => (elem.innerText = elem.dataset.color), 750);
+};
 
 const showColors = () => {
   colorList.innerHTML = pickedColors
@@ -9,23 +14,32 @@ const showColors = () => {
       <span class="rect" style="background: ${color}; border: 1px solid ${
         color == "#ffffff" ? "#ccc" : color
       }"></span>
-      <span class="value">${color}</span>
+      <span class="value" data-color="${color}">${color}</span>
     </li>`
     )
-    .join("");
+    .join(""); //generates li for each picked color and adds it to list
+  //copy hexcode on click
+  document.querySelectorAll(".color").forEach((li) => {
+    li.addEventListener("click", (e) =>
+      copyColor(e.currentTarget.lastElementChild)
+    );
+  });
 };
 showColors();
 
 const activateEyeDropper = async () => {
   try {
+    //open eyedropper and get color
     const eyeDropper = new EyeDropper();
     const { sRGBHex } = await eyeDropper.open();
     navigator.clipboard.writeText(sRGBHex);
 
-    pickedColors.push(sRGBHex);
-    console.log(pickedColors);
-    localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
-    showColors();
+    //add color to list if its not already in the list (no duplicates)
+    if (!pickedColors.includes(sRGBHex)) {
+      pickedColors.push(sRGBHex);
+      localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+      showColors();
+    }
   } catch (error) {
     console.log(error);
   }
